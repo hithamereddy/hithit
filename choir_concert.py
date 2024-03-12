f9056e6521d1bdfcc835940476391a0f1ed67891
Hitha Mereddy <79064191+hithamereddy@users.noreply.github.com>
2024-03-12 09:49:29 -0700

    Create choir_concert.py

---
+++ choir_concert.py
@@ -0,0 +1,8 @@
+attending_concert = input("are you attending the choir concert? enter y/n: ")
+
+if attending_concert == "y":
+  free_after = input("are you free to hang out for a little after the concert? enter y/n: ")
+  if free_after == "y":
+    print("yippee!!!")
+  else:
+    print("aw")
